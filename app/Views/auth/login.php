30a01ef7039156ff210de8ca3579de9237a3c22a
igor <igorsxb.am@gmail.com>
2025-10-20 00:20:44 -0300

    correção links css para ambiente de produção

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -9,7 +9,7 @@ if (!defined('BASE_URL')) exit;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reframax - Portal de Acesso</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style.css"> 
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
     <style>
--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -8,7 +8,7 @@ if (!defined('BASE_URL')) exit;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reframax | Sistema Gestivo</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
